435d127be02e6c04c55c20784bd14a5e5834b64c
xxfast <isurukusumal36@gmail.com>
2023-04-24 13:06:57 +0200

    Remove unnecessary top level build plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
-plugins {
-    //trick: for the same plugin versions in all sub-modules
-    id("com.android.application") version "7.3.1" apply false
-    id("com.android.library") version "7.3.1" apply false
-    kotlin("android") version "1.8.0" apply false
-    kotlin("multiplatform") version "1.8.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
-}
+plugins { }
 
 buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+
     dependencies {
+        classpath(libs.agp)
         classpath(libs.compose.multiplatform)
         classpath (libs.kotlin.gradle.plugin)
         classpath(libs.kotlin.serialization)
