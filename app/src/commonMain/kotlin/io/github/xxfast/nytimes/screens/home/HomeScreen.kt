4cac9671d5a8c1ddf69fa456999f689fd5933697
xxfast <isurukusumal36@gmail.com>
2023-07-04 14:07:51 +1000

    Enable predictive back stack animation for home screen

--- app/src/commonMain/kotlin/io/github/xxfast/nytimes/screens/home/HomeScreen.kt
+++ app/src/commonMain/kotlin/io/github/xxfast/nytimes/screens/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package io.github.xxfast.nytimes.screens.home
 
 import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.predictiveBackAnimation
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
+import io.github.xxfast.decompose.LocalComponentContext
 import io.github.xxfast.decompose.router.Router
 import io.github.xxfast.decompose.router.content.RoutedContent
 import io.github.xxfast.decompose.router.rememberRouter
@@ -13,13 +16,18 @@ import io.github.xxfast.nytimes.screens.home.StoryHomeScreen.List
 import io.github.xxfast.nytimes.screens.story.StoryScreen
 import io.github.xxfast.nytimes.screens.topStories.TopStoriesScreen
 
+@OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun HomeScreen() {
   val router: Router<StoryHomeScreen> = rememberRouter(StoryHomeScreen::class, listOf(List))
 
   RoutedContent(
     router = router,
-    animation = stackAnimation(slide()),
+    animation = predictiveBackAnimation(
+      backHandler = LocalComponentContext.current.backHandler,
+      onBack = { router.pop() },
+      animation = stackAnimation(slide())
+    ),
   ) { screen ->
     when (screen) {
       List -> TopStoriesScreen(
