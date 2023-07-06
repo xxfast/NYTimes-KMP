3e079846714e71ffd0399963804e5460d2f9c331
xxfast <isurukusumal36@gmail.com>
2023-07-07 09:15:49 +1000

    Fix predictive back on ios target

--- app/ios-compose/src/iosMain/kotlin/io/github/xxfast/nytimes/ios/compose/Application.kt
+++ app/ios-compose/src/iosMain/kotlin/io/github/xxfast/nytimes/ios/compose/Application.kt
@@ -1,12 +1,21 @@
 package io.github.xxfast.nytimes.ios.compose
 
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.LocalUIViewController
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.jetbrains.PredictiveBackGestureIcon
+import com.arkivanov.decompose.extensions.compose.jetbrains.PredictiveBackGestureOverlay
+import com.arkivanov.essenty.backhandler.BackDispatcher
+import com.arkivanov.essenty.backhandler.BackHandler
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import io.github.xxfast.decompose.LocalComponentContext
 import io.github.xxfast.nytimes.di.appStorage
@@ -17,9 +26,16 @@ import io.github.xxfast.androidx.compose.material3.windowsizeclass.calculateWind
 import platform.Foundation.NSHomeDirectory
 import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalDecomposeApi::class)
 fun Main(): UIViewController = ComposeUIViewController {
   val lifecycle = LifecycleRegistry()
-  val rootComponentContext = DefaultComponentContext(lifecycle = lifecycle)
+  val backDispatcher = BackDispatcher()
+
+  val rootComponentContext = DefaultComponentContext(
+    lifecycle = lifecycle,
+    backHandler = backDispatcher
+  )
+
   appStorage = NSHomeDirectory()
 
   /**
@@ -32,7 +48,18 @@ fun Main(): UIViewController = ComposeUIViewController {
       LocalWindowSizeClass provides windowSizeClass,
     ) {
       MaterialTheme {
-        HomeScreen()
+        PredictiveBackGestureOverlay(
+          backDispatcher = backDispatcher, // Use the same BackDispatcher as above
+          backIcon = { progress, _ ->
+            PredictiveBackGestureIcon(
+              imageVector = Icons.Default.ArrowBack,
+              progress = progress,
+            )
+          },
+          modifier = Modifier.fillMaxSize(),
+        ) {
+          HomeScreen()
+        }
       }
     }
   }
