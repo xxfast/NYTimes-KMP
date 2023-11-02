e933558563a0ba4e1619304a4ecb8b4bb58d0c16
xxfast <isurukusumal36@gmail.com>
2023-11-02 14:39:05 +1100

    Add ios application

--- app/src/iosMain/kotlin/io/github/xxfast/nytimes/Application.kt
+++ app/src/iosMain/kotlin/io/github/xxfast/nytimes/Application.kt
@@ -1,4 +1,4 @@
-package io.github.xxfast.nytimes.ios.compose
+package io.github.xxfast.nytimes
 
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,32 +10,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.LocalUIViewController
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.ComposeUIViewController
-import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.PredictiveBackGestureIcon
 import com.arkivanov.decompose.extensions.compose.jetbrains.PredictiveBackGestureOverlay
 import com.arkivanov.essenty.backhandler.BackDispatcher
-import com.arkivanov.essenty.backhandler.BackHandler
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-import io.github.xxfast.decompose.LocalComponentContext
-import io.github.xxfast.nytimes.di.appStorage
-import io.github.xxfast.nytimes.screens.home.HomeScreen
 import io.github.xxfast.androidx.compose.material3.windowsizeclass.LocalWindowSizeClass
 import io.github.xxfast.androidx.compose.material3.windowsizeclass.WindowSizeClass
 import io.github.xxfast.androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import io.github.xxfast.decompose.router.LocalRouterContext
+import io.github.xxfast.decompose.router.RouterContext
+import io.github.xxfast.nytimes.di.appStorage
+import io.github.xxfast.nytimes.screens.home.HomeScreen
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.autoreleasepool
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toCValues
 import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSStringFromClass
+import platform.UIKit.UIApplicationMain
 import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+fun main() {
+  val args = emptyArray<String>()
+  memScoped {
+    val argc = args.size + 1
+    val argv = (arrayOf("skikoApp") + args).map { it.cstr.ptr }.toCValues()
+    autoreleasepool {
+      UIApplicationMain(argc, argv, null, NSStringFromClass(AppDelegate))
+    }
+  }
+}
+
 @OptIn(ExperimentalDecomposeApi::class)
-fun Main(): UIViewController = ComposeUIViewController {
-  val lifecycle = LifecycleRegistry()
-  val backDispatcher = BackDispatcher()
-
-  val rootComponentContext = DefaultComponentContext(
-    lifecycle = lifecycle,
-    backHandler = backDispatcher
-  )
-
+fun HomeUIViewController(routerContext: RouterContext): UIViewController = ComposeUIViewController {
   appStorage = NSHomeDirectory()
 
   /**
@@ -44,12 +54,12 @@ fun Main(): UIViewController = ComposeUIViewController {
   BoxWithConstraints {
     val windowSizeClass: WindowSizeClass = calculateWindowSizeClass(DpSize(maxWidth, maxHeight))
     CompositionLocalProvider(
-      LocalComponentContext provides rootComponentContext,
+      LocalRouterContext provides routerContext,
       LocalWindowSizeClass provides windowSizeClass,
     ) {
       MaterialTheme {
         PredictiveBackGestureOverlay(
-          backDispatcher = backDispatcher, // Use the same BackDispatcher as above
+          backDispatcher = routerContext.backHandler as BackDispatcher, // Use the same BackDispatcher as above
           backIcon = { progress, _ ->
             PredictiveBackGestureIcon(
               imageVector = Icons.Default.ArrowBack,
